39ea94c1cc3a0befbae13880112ae67d47da065f
PolRodriguez-CTS <prodriguez@ctsformacion.com>
2025-03-19 11:36:26 +0100

    camera script

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    //Variable donde leeremos las coordenadas del jugador
+    public Transform playerTransform;
+    //variable tipo vector con tres componentes (x,y,z) para hacer un offset a la cámara
+    public Vector3 offset;
+    //Posición máxmima y mínima que podrán tener los valores X, Y del transform de la cámara
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+
+    public float interpolationRatio = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Queremos que la posición de la cámara sea igual a la del jugador (Mario) + el offset
+        Vector3 desiredPosition = playerTransform.position + offset;
+
+        //Creamos los límites de la posición en X e Y de la cámara
+        float clampX = Mathf.Clamp(desiredPosition.x, minPosition.x, maxPosition.x);
+        float ClampY = Mathf.Clamp(desiredPosition.y, minPosition.y, maxPosition.y);
+        Vector3 clampedPosition = new Vector3(clampX, ClampY, desiredPosition.z);
+
+        Vector3 lerpedPosition = Vector3.Lerp(transform.position, clampedPosition, interpolationRatio);
+        //Hacemos que la posición ahora equivalga a clamped position, Clammped position = posición del jugador + el offset + los límites
+        transform.position = lerpedPosition;
+    }
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,15 +6,21 @@ public class PlayerControl : MonoBehaviour
 {
     public float playerSpeed = 4.5f;
     public int playerDirection = 1;
+    //almacena si pulsamos el boton para movernos
     private float inputHorizontal;
+    //variable de componente
     private Rigidbody2D rigidBody;
-    public float jumpForce = 10;
+    public float jumpForce = 15;
     public GroundSensor _groundSensor;
+
+    public SpriteRenderer _spriteRenderer;
     
+    //función de Unity que se llama sola
     void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         _groundSensor = GetComponentInChildren<GroundSensor>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
     
     // Start is called before the first frame update
@@ -28,6 +34,7 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //este input lo podemos encontrar en Unity como bindeo de tecla de los controles del jugador.
         inputHorizontal = Input.GetAxisRaw("Horizontal");
         //Mover automáticamente en X --> playerSpeed = velocidad    playerDirection = dirección en la que se mueve
         //transform.position = new Vector3 (transform.position.x + playerDirection * playerSpeed * Time.deltaTime, transform.position.y, transform.position.z);
@@ -38,10 +45,11 @@ public class PlayerControl : MonoBehaviour
         //Move towards hace que el objeto vaya de un punto a otro
         //transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x + inputHorizontal, transform.position.y), playerSpeed * Time.deltaTime);
         //condición para el salto
-        if(Input.GetButtonDown("Jump") && !_groundSensor.isGrounded == false)
+        if(Input.GetButtonDown("Jump") && _groundSensor.isGrounded == true)
         {
-            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            Jump();
         }
+        Movement();
     }
 
     void FixedUpdate()
@@ -50,5 +58,24 @@ public class PlayerControl : MonoBehaviour
         rigidBody.velocity = new Vector2(inputHorizontal*playerSpeed, rigidBody.velocity.y);
         //rigidBody.AddForce(new Vector2(inputHorizontal,0));
         //rigidBody.MovePosition(new Vector2(100,0));
+    }
+
+        //Función personalizada para hacer que mario mire hacia un lado u otro dependiendo de hacia dónde se mueva
+    void Movement()
+    {
+        //Si el input horizontal es positivo, en el componente SpriteRenderer, Flip X no se activa, si el input horizontal es negativo, se activa
+         if(inputHorizontal > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if(inputHorizontal < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+    }
+
+    void Jump()
+    {
+        rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
